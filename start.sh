ae49ac21fb44b40412411b4147718651adc2dcec
Marah-uwase <maranahuwase12@gmail.com>
2020-12-17 00:41:12 +0200

    exporting email

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-export MAIL_USERNAME='erastuskariuki15@gmail.com'
-export MAIL_PASSWORD='@e1r2a3s4#'
-export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://erastus:Angular2020@localhost/homespace'
+# export MAIL_USERNAME='maranahuwase12@gmail.com'
+# export MAIL_PASSWORD='marahuwase@31998'
+export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maranatha:marah@localhost/homespace'
 
 
 
