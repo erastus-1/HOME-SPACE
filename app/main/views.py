820f642b7b5f574045f1b5ef5601d48d7987f1c5
Marah-uwase <maranahuwase12@gmail.com>
2020-12-17 00:26:36 +0200

    registering blueprints

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,89 @@
-from flask import render_template, url_for
+from flask import render_template,redirect,url_for, abort,request,flash
 from . import main
+# from ..requests import get_house
+from .forms import CommentsForm, EditProfileForm
+from ..models import User,House, Language, Comment
+from flask_login import login_required, current_user
+from .. import db, photos
+# import markdown2
+from ..email import mail_message
 
-@main.route('/')
+
+
+    
+
+@main.route('/index')
 def index():
 
-    title = 'Home-  Welcome to The House Space Website'
-    return render_template('index.html', title = title)
+	title = 'Home-  Welcome to The House Space Website'
+	return render_template('index.html', title = title)
+
+	page = request.args.get('page', 1, type=int)
+	Houses = current_user.houses.order_by(House.timestamp.desc()).paginate(
+		page, current_app.config['POSTS_PER_PAGE'], False)
+	next_url = url_for('.index', page=houses.next_num) \
+		if houses.has_next else None
+	prev_url = url_for('.index', page=houses.prev_num) \
+		if houses.has_prev else None
+	return render_template('index.html', title='Home', houses=houses.items,
+						   next_url=next_url, prev_url=prev_url)
+
+
+@main.route('/user/<username>', methods=['GET', 'POST'])
+@login_required
+def user(username):
+	user = User.query.filter_by(username=username).first_or_404()
+	
+	return render_template('user.html', title='User Profile', user=user)
+
+
+@main.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+	if current_app.config['ADMIN_LOCKED']:
+		super_admin = User.query.filter_by(username='admin').first()
+	if current_user == super_admin:
+		flash('Super admin\'s profile is locked')
+		return redirect(url_for('.user', username=current_user.username))
+	form = EditProfileForm(current_user.username)
+	if form.validate_on_submit():
+		current_user.username = form.username.data
+		language = Language.query.get(int(form.language.data))
+		if language:
+			current_user.language = language
+		db.session.commit()
+		flash('Your changes have been saved.')
+		return redirect(url_for('.edit_profile'))
+	elif request.method == 'GET':
+		form.username.data = current_user.username
+		form.language.data = current_user.language_id
+	return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+@main.route('/about')
+def about():
+    '''
+    View root page function that returns the about page and its data
+    '''
+    title = ''
+    return render_template('about.html', title=title)
+
+
+@main.route('/contact')
+def contact():
+    '''
+    View root page function that returns the contact page and its data
+    '''
+    title = ''
+    return render_template('contact.html', title = title)
+
+@main.route('/house/comments/new/<int:id>',methods = ['GET','POST'])
+@login_required
+def new_comment(id):
+    form = CommentsForm()
+   
+    if form.validate_on_submit():
+        new_comment = Comment(house_id =id,comment=form.comment.data)
+        new_comment.save_comments()
+        return redirect(url_for('main.post',post_id=id))
+    
+    return render_template('new_comment.html',comment_form=form)
