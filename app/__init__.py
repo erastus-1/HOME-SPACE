55096058188bb2bd30d1ce0116a95a45227bb86a
erastus <erastuskariuki15@gmail.com>
2020-12-16 09:46:08 +0300

    add merged files

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
-<<<<<<< HEAD
 from flask_sqlalchemy import SQLAlchemy
 
 bootstrap = Bootstrap()
@@ -10,14 +9,6 @@ db = SQLAlchemy()
 def create_app(config_name):
     app = Flask(__name__)
 
-=======
-
-bootstrap = Bootstrap()
-
-def create_app(config_name):
-    app = Flask(__name__)
-    
->>>>>>> 86754da7c3d65fd27868b52f7533b96490a3f83e
     app.config.from_object(config_options[config_name])
 
     bootstrap.init_app(app)
@@ -25,8 +16,4 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-<<<<<<< HEAD
     return app 
-=======
-    return app
->>>>>>> 86754da7c3d65fd27868b52f7533b96490a3f83e
