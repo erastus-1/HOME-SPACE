c5d7c535b95df4cf7f0d4f8850c23f99319a099a
Marah-uwase <maranahuwase12@gmail.com>
2020-12-17 08:10:29 +0200

    modify the start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
-# export MAIL_USERNAME='maranahuwase12@gmail.com'
-# export MAIL_PASSWORD='marahuwase@31998'
-export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maranatha:marah@localhost/homespace'
+export SECRET_KEY= 'SECRET_KEY'
 
 
-
-python3.8 manage.py server
+python3.6 manage.py server
