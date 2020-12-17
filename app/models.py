1b2c2d99aa4b305f57f3e9d32080083770503f69
Marah-uwase <maranahuwase12@gmail.com>
2020-12-17 13:17:32 +0200

    importing relesfor db

--- app/models.py
+++ app/models.py
@@ -47,6 +47,61 @@ class User( UserMixin, db.Model):
     def __repr__(self):
         return f'User {self.username}'
 
+class Permission:
+	USER = 1
+	ADMIN = 2
+
+class Role(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), unique=True)
+	default = db.Column(db.Boolean, default=False, index=True)
+	permissions = db.Column(db.Integer)
+	users = db.relationship('User', backref='role', lazy='dynamic')
+
+	def __repr__(self):
+		return '<Role {}>'.format(self.name)
+
+	def __init__(self, **kwargs):
+		super(Role, self).__init__(**kwargs)
+		if self.permissions is None:
+			self.permissions = 0
+
+	@staticmethod
+	def insert_roles():
+		roles = {
+			'User': [Permission.USER],
+			'Administrator': [Permission.USER, Permission.ADMIN]
+		}
+		default_role = 'User'
+		for r in roles:
+			role = Role.query.filter_by(name=r).first()
+			if role is None:
+				role = Role(name=r)
+			role.reset_permissions()
+			for perm in roles[r]:
+				role.add_permission(perm)
+			role.default = (role.name == default_role)
+			db.session.add(role)
+		db.session.commit()
+
+	def add_permission(self, perm):
+		if not self.has_permission(perm):
+			self.permissions += perm
+
+	def remove_permission(self, perm):
+		if self.has_permission(perm):
+			self.permissions -= perm
+
+	def reset_permissions(self):
+		self.permissions = 0
+
+	def has_permission(self, perm):
+		return self.permissions & perm == perm
+
+	def get_name(self):
+		return self.name
+
+
 class AnonymousUser(AnonymousUserMixin):
     __tablename__ = 'user'
 
@@ -143,16 +198,16 @@ class HouseLanguage(db.Model):
     def get_language_code(self):
         return self.language.code
 
-class Role(db.Model):
-    __tablename__ = 'role'
+# class Role(db.Model):
+#     __tablename__ = 'role'
 
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
-    users = db.relationship('User',backref = 'role',lazy="dynamic")
+#     id = db.Column(db.Integer,primary_key = True)
+#     name = db.Column(db.String(255))
+#     users = db.relationship('User',backref = 'role',lazy="dynamic")
 
 
-    def __repr__(self):
-        return f'User {self.name}'
+#     def __repr__(self):
+#         return f'User {self.name}'
 
 class Comment(db.Model):
     __tablename__ = 'comments'
